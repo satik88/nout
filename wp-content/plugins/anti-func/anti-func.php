56a44f46a918513cc3f463fb43f8912d17201138
satik88 <86455351+satik88@users.noreply.github.com>
2021-06-30 15:13:21 +0300

    Отключаем табы

--- wp-content/plugins/anti-func/anti-func.php
+++ wp-content/plugins/anti-func/anti-func.php
@@ -321,7 +321,8 @@ function my_remove_product_result_count()
 	remove_action( 'woocommerce_before_single_product', 'woocommerce_output_all_notices', 10 );	
 	remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_images', 20 );
 	remove_action( 'woocommerce_product_thumbnails', 'woocommerce_show_product_thumbnails', 20 );
-	
+	//Отключаем табы
+	remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
 
 }
 //хук хлебных крошек
