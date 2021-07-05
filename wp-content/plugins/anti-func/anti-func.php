1340658e6c5b6f305438c704f9174a58d245599c
satik88 <86455351+satik88@users.noreply.github.com>
2021-07-05 10:54:51 +0300

    установили плагин Custom Product Tabs for WooComm

--- wp-content/plugins/anti-func/anti-func.php
+++ wp-content/plugins/anti-func/anti-func.php
@@ -359,3 +359,13 @@ add_action( 'woocommerce_single_variation22', 'woocommerce_single_variation22', 
 function woocommerce_single_variation22() {
 	echo '<div class="woocommerce-variation single_variation"></div>';
 }
+add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 );
+ 
+function woo_remove_product_tabs( $tabs ) {
+ 
+    unset( $tabs['description'] );      	// Remove the description tab
+    unset( $tabs['reviews'] ); 			// Remove the reviews tab
+    unset( $tabs['additional_information'] );  	// Remove the additional information tab
+ 
+    return $tabs;
+}
